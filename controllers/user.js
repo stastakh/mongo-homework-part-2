4a109701bb1611214dca4a55a39a808c265447d3
stastakh <stastakh@gmail.com>
2019-12-08 18:19:27 +0200

    add logic for getting user by his ID

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,6 @@
 const userModel = require('../models/user');
 
-module.exports = { createUser, updateUser };
+module.exports = { createUser, updateUser, getUserById };
 
 async function createUser(req, res, next) {
   const { body } = req;
@@ -17,6 +17,16 @@ async function updateUser(req, res, next) {
   try {
     const updatedUser = await userModel.updateOne({ _id: params.id }, body);
     res.json(updatedUser);
+  } catch (err) {
+    throw err;
+  }
+}
+
+async function getUserById(req, res, next) {
+  const { params } = req;
+  try {
+    const user = await userModel.findOne({ _id: params.id });
+    res.json(user);
   } catch (err) {
     throw err;
   }
--- routes/user.route.js
+++ routes/user.route.js
@@ -7,4 +7,6 @@ router.post('/', userController.createUser);
 
 router.put('/:id', userController.updateUser);
 
+router.get('/:id', userController.getUserById);
+
 module.exports = router;
