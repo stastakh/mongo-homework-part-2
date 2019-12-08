c81e298fb140cbcfe844673610189ddbea383ea0
stastakh <stastakh@gmail.com>
2019-12-08 23:38:50 +0200

    add validation to the create and update article

--- app.js
+++ app.js
@@ -21,7 +21,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/api', api);
 
 app.use((err, req, res, next) => {
-  console.log(err);
   if (err.name === 'CastError') {
     res.status(404).send(err.message);
   } else {
